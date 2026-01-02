4773f1c8f802be33838f3e69c0852753c6ee0531
sesetty bhargavi <sesettybhargavi@gmail.com>
2026-01-02 22:12:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+	const fname = document.forms["form1"]["fname"].value;
+	const lname = document.forms["form1"]["lname"].value;
 
+	alert(fname + " " + lname);
 }
